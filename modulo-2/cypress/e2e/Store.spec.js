008b764f3b58b0d571012dec423b3b07cf9d62a3
= <alexandressoares383@gmail.com>
2023-07-15 16:26:16 -0300

    feat: test product list

--- modulo-2/cypress/e2e/Store.spec.js
+++ modulo-2/cypress/e2e/Store.spec.js
@@ -15,15 +15,42 @@ context('Store', () => {
   });
 
   it('should display the store', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
 
     cy.get('body').contains('Brand');
     cy.get('body').contains('Wrist Watch');
   });
 
+  context('Store > Product List', () => {
+    it('should display "0 Products" when no products is returned', () => {
+      cy.window().visit('/');
+
+      cy.get('[data-testid="product-card"]').should('have.length', 0);
+      cy.get('body').contains('0 Products');
+    });
+
+    it('should display "1 Product" when 1 products is returned', () => {
+      server.create('product');
+
+      cy.window().visit('/');
+
+      cy.get('[data-testid="product-card"]').should('have.length', 1);
+      cy.get('body').contains('1 Product');
+    });
+
+    it('should display "10 Products" when 10 products are returned', () => {
+      server.createList('product', 10);
+
+      cy.window().visit('/');
+
+      cy.get('[data-testid="product-card"]').should('have.length', 10);
+      cy.get('body').contains('10 Products');
+    });
+  });
+
   context('Store > Search for Products', () => {
     it('should type in the search field', () => {
-      cy.window().visit('http://localhost:3000');
+      cy.window().visit('/');
 
       cy.window()
         .get('input[type="search"]')
@@ -37,7 +64,7 @@ context('Store', () => {
       });
       server.createList('product', 10);
 
-      cy.window().visit('http://localhost:3000');
+      cy.window().visit('/');
       cy.window().get('input[type="search"]').type('Computador Novo');
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should('have.length', 1);
@@ -46,7 +73,7 @@ context('Store', () => {
     it('should not return any product', () => {
       server.createList('product', 10);
 
-      cy.window().visit('http://localhost:3000');
+      cy.window().visit('/');
       cy.window().get('input[type="search"]').type('Computador Novo');
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should('have.length', 0);
