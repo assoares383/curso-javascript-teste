c31ce418346fdef2bc0d42d8aa0cf9dd61e5028c
= <alexandressoares383@gmail.com>
2023-07-15 17:03:59 -0300

    feat: test opening and closing of the shopping cart

--- modulo-2/cypress/e2e/Store.spec.js
+++ modulo-2/cypress/e2e/Store.spec.js
@@ -21,6 +21,28 @@ context('Store', () => {
     cy.get('body').contains('Wrist Watch');
   });
 
+  context('Store > Shopping Card', () => {
+    it('should not display shopping cart when page first loads', () => {
+      cy.visit('/');
+
+      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden');
+    });
+
+    it.only('should toggle shopping cart visibility when button is clicked', () => {
+      cy.visit('/');
+      cy.get('[data-testid="toggle-button"]').as('toggleButton');
+
+      cy.get('@toggleButton').click();
+      cy.get('[data-testid="shopping-cart"]').should(
+        'not.have.class',
+        'hidden'
+      );
+
+      cy.get('@toggleButton').click({ force: true });
+      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden');
+    });
+  });
+
   context('Store > Product List', () => {
     it('should display "0 Products" when no products is returned', () => {
       cy.window().visit('/');
