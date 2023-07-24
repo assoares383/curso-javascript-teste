66d3c91c3f173d00b7894091042ba46c9054df17
= <alexandressoares383@gmail.com>
2023-07-24 19:28:56 -0300

    feat: remove product, clean cart and a new unit test

--- modulo-2/cypress/e2e/Store.spec.js
+++ modulo-2/cypress/e2e/Store.spec.js
@@ -23,7 +23,7 @@ context('Store', () => {
     g('body').contains('Wrist Watch');
   });
 
-  context.only('Store > Shopping Card', () => {
+  context('Store > Shopping Card', () => {
     const quantity = 10;
 
     beforeEach(() => {
@@ -37,12 +37,24 @@ context('Store', () => {
 
     it('should toggle shopping cart visibility when button is clicked', () => {
       gid('toggle-button').as('toggleButton');
-
       g('@toggleButton').click();
+
       gid('shopping-cart').should('not.have.class', 'hidden');
 
       g('@toggleButton').click({ force: true });
       gid('shopping-cart').should('have.class', 'hidden');
+    });
+
+    it.only('should not display "Clear cart" button when cart is empty', () => {
+      gid('toggle-button').as('toggleButton');
+      g('@toggleButton').click();
+      gid('clear-cart-button').should('not.be.visible');
+    });
+
+    it('should display "Cart is empty" message when there are no products', () => {
+      gid('toggle-button').as('toggleButton');
+      g('@toggleButton').click();
+      gid('shopping-cart').contains('Cart is empty');
     });
 
     it('should open shopping cart when a product is added', () => {
@@ -71,6 +83,24 @@ context('Store', () => {
       cy.addToCart({ indexes: 'all' });
 
       gid('cart-item').should('have.length', quantity);
+    });
+
+    it('should remove a product from cart', () => {
+      cy.addToCart({ index: 2 });
+
+      gid('cart-item').as('cartItems');
+
+      g('@cartItems').should('have.length', 1);
+      g('@cartItems').first().find('[data-testid="remove-button"]').click();
+      g('@cartItems').should('have.length', 0);
+    });
+
+    it('should clear cart when "Clear cart" button is clicked', () => {
+      cy.addToCart({ indexes: [1, 2, 3] });
+
+      gid('cart-item').should('have.length', 3);
+      gid('clear-cart-button').click();
+      gid('cart-item').should('have.length', 0);
     });
   });
 
